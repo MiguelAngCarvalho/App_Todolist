031c2900c24eee7b2ae554cfe1d57a545e31688d
Miguel <miguel.aavcarvalho@gmail.com>
2024-11-17 17:42:45 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:app_todolist/app/app_to_do_list.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const AppToDoList());
 }
